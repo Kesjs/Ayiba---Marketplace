19749d379047b3496ef201531d1206e7db8d9a6c
Ken ken <95589932+Kesjs@users.noreply.github.com>
2026-07-25 07:32:20 +0100

    Add files via upload

---
+++ marketplace/app/centre-aide/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Navbar } from "@/components/ui/Navbar";
+import { Footer } from "@/components/home/Footer";
+import { CentreAideContent } from "@/components/legal/CentreAideContent";
+
+export default function CentreAidePage() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col min-h-screen bg-gray-50/50">
+        <div className="flex-1 py-16 px-4 md:px-8 lg:px-12">
+          <CentreAideContent />
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}

3786c87618845c8884e9b0a5168a4416361f1a1e
Ken ken <95589932+Kesjs@users.noreply.github.com>
2026-07-28 04:43:46 +0100

    Add files via upload

--- marketplace/app/privacy/page.tsx
+++ marketplace/app/privacy/page.tsx
@@ -3,6 +3,7 @@
 import { Navbar } from "@/components/ui/Navbar";
 import { Footer } from "@/components/home/Footer";
 import { PrivacyContent } from "@/components/legal/PrivacyContent";
+import { LegalNotice } from "@/components/legal/LegalNotice";
 
 export default function PrivacyPage() {
   return (
@@ -10,6 +11,7 @@ export default function PrivacyPage() {
       <Navbar />
       <div className="flex flex-col min-h-screen bg-gray-50/50">
         <div className="flex-1 py-16 px-4 md:px-8 lg:px-12">
+          <LegalNotice paramKey="confidentialite_note_admin" />
           <PrivacyContent />
         </div>
       </div>
